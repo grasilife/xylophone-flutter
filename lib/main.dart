4a6a04df4e4ad6ea42120d6d4c4b53d4160200c8
fuguoqiang <434543162@qq.com>
2022-04-30 00:45:23 +0800

    函数组件

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,18 @@ class XylophoneApp extends StatelessWidget {
     player.play('note$soundNumber.wav');
   }
 
+  Expanded buildkey() {
+    return Expanded(
+      child: TextButton(
+        style: TextButton.styleFrom(backgroundColor: Colors.orange),
+        onPressed: () {
+          playSound(1);
+        },
+        child: null,
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -17,14 +29,18 @@ class XylophoneApp extends StatelessWidget {
         backgroundColor: Colors.black,
         body: SafeArea(
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              TextButton(
-                style: TextButton.styleFrom(backgroundColor: Colors.red),
-                onPressed: () {
-                  playSound(1);
-                },
-                child: null,
-              )
+              buildkey(),
+              Expanded(
+                child: TextButton(
+                  style: TextButton.styleFrom(backgroundColor: Colors.red),
+                  onPressed: () {
+                    playSound(1);
+                  },
+                  child: null,
+                ),
+              ),
             ],
           ),
         ),
